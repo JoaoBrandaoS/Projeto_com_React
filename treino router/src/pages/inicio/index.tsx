03884b7de2b6e5abe70c10e363bddf867ba5cf5a
João Vitor Brandão <105953676+JoaoBrandaoS@users.noreply.github.com>
2023-02-23 09:42:59 -0300

    Adicionando treino usando React-Router-Dom

---
+++ treino router/src/pages/inicio/index.tsx
@@ -0,0 +1,32 @@
+import Menu from 'components/menu';
+import cardapio from 'data/cardapio.json';
+import styles from './inicio.module.scss';
+
+export default function Inicio(){
+    
+    let pratosRecomendados = [...cardapio];
+    
+    pratosRecomendados = pratosRecomendados.sort(() => 0.5 - Math.random()).splice(0,3);
+
+    return(
+        <section>
+            <Menu />
+            <h3 className={styles.titulo}>
+            Recomendações da cozinha
+            </h3>
+            <div className={styles.recomendados}>
+                {pratosRecomendados.map(item => (
+                    <div key={item.id} className ={styles.recomendado}>
+                        <div className ={styles.recomendado_imagem}>                 
+                            <img src={item.photo} alt={item.title}/>    
+                        </div>
+                        <button className={styles.recomendado__botao}>
+                Ver mais
+                        </button>
+                    </div>
+            
+                ))}
+            </div>
+        </section>
+    );
+}
